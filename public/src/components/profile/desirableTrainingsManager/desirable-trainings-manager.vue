b6a9141cd5bc360aa300e1714c8447718390ebe6
Shalva Aroshidze <shalva.aroshidze@gmail.com>
2018-04-23 19:52:00 +0400

    skeleton created of desirable trainings

---
+++ public/src/components/profile/desirableTrainingsManager/desirable-trainings-manager.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="desirable-trainings-manager">
+  <b-card :title="cardTitle">
+    <b-button
+      v-if="!sectionOpen"
+      size="lg"
+      variant="success"
+      @click="openSection"
+    >
+        დაამატე სასურველი ტრეინინგები
+    </b-button>
+    <desirable-trainings-section v-if="sectionOpen" />
+  </b-card>
+</div>
+</template>
+
+<script>
+import desirableTrainingsSection from './desirable-trainings-section'
+
+export default {
+  name: 'desirable-trainings-manager',
+  data: () => ({
+    sectionOpen: false
+  }),
+  methods: {
+    openSection() {
+      this.sectionOpen = true
+    }
+  },
+  components: {
+    'desirable-trainings-section': desirableTrainingsSection
+  },
+  computed: {
+    cardTitle () {
+      return this.sectionOpen ? 'სასურველი ტრეინინგები' : null
+    },
+    suggestions () {
+      if (!this.synchronous) return this.list
+
+      return this.list.filter(item => item.includes(this.inputValue))
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ public/src/components/profile/desirableTrainingsManager/desirable-trainings-section.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="desirable-trainings-section">
+  bom bom
+</div>
+</template>
+
+<script>
+export default {
+  name: 'desirable-trainings-section',
+  data: () => ({
+  }),
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
