f4a1ae750cfb81ad5e57bdd01ff0d675ca35ca4d
Shalva Aroshidze <shalva.aroshidze@gmail.com>
2018-04-24 18:46:09 +0400

    nav bar basic done

--- public/src/components/nav-bar.vue
+++ public/src/components/nav-bar.vue
@@ -1,46 +1,26 @@
 <template>
-  <b-navbar toggleable="md" type="dark" variant="info">
+  <b-navbar toggleable="md" type="dark" variant="dark">
 
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-    <b-navbar-brand href="#">Worknet</b-navbar-brand>
+    <b-navbar-brand href="#">WORKNET</b-navbar-brand>
 
     <b-collapse is-nav id="nav_collapse">
-
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item href="#/vacancies">ვაკანსიები</b-nav-item>
+        <b-nav-item v-if="loggedIn" href="#/profile">პროფილი</b-nav-item>
       </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
+        <b-navbar-nav>
+          <b-nav-item v-if="!loggedIn" href="#/login">ავტორიზაცია</b-nav-item>
+          <b-nav-item v-if="!loggedIn" href="#/register">რეგისტრაცია</b-nav-item>
+        </b-navbar-nav>
 
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using button-content slot -->
-          <template slot="button-content">
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Signout</b-dropdown-item>
+        <b-nav-item-dropdown text="" right>
+          <b-dropdown-item v-if="loggedIn" @click="logout">გასვლა</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
-
-
-      <b-button v-if="loggedIn" variant="primary" @click="logout">
-        გასვლა
-      </b-button>
     </b-collapse>
   </b-navbar>
 </template>
@@ -67,7 +47,7 @@ export default {
   },
   methods: {
     logout() {
-      Cookies.remove('token') 
+      Cookies.remove('token')
 
       this.loggedIn = false
 
