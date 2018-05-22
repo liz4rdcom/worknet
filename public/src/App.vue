366152ea060f809d2db29d8a846b67105d2d4866
Shalva Aroshidze <shalva.aroshidze@gmail.com>
2018-05-22 14:35:12 +0400

    scss

--- public/src/App.vue
+++ public/src/App.vue
@@ -19,20 +19,19 @@
   }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    background: whitesmoke;
-    height: 100%;
-    overflow-y: auto;
-  }
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: red;
+  height: 100%;
+  overflow-y: auto;
+}
 
-  .flex {
-    flex: 1;
-  }
-
+.flex {
+  flex: 1;
+}
 </style>
